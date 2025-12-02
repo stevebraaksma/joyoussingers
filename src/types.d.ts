7ec146d4b5b3cc4d8680b9b16f9e9b399872bf13
Steve Braaksma <stevebraaksma@hotmail.com>
2025-12-01 21:34:37 -0600

    christmas concert update

--- src/types.d.ts
+++ src/types.d.ts
@@ -218,6 +218,7 @@ export interface Hero extends Omit<Headline, 'classes'>, Omit<Widget, 'isDark' |
   content?: string;
   actions?: string | CallToAction[];
   image?: string | unknown;
+  image2?: string | unknown;
 }
 
 export interface Team extends Omit<Headline, 'classes'>, Widget {
